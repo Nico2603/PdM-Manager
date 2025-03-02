a6b6e707536b69d69e3c3dd7c9483a5de67d94b2
Nico2603 <nicolasceballosbrito@gmail.com>
2025-03-02 17:33:32 -0500

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,19 +1,14 @@
-# app/database.py
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import os
 
-# Variables de conexión (puedes modificar estos valores o cargarlos desde el entorno)
-DB_USER = os.getenv('DB_USER', 'root')
-DB_PASSWORD = os.getenv('DB_PASSWORD', '')
-DB_HOST = os.getenv('DB_HOST', '127.0.0.1')
-DB_PORT = os.getenv('DB_PORT', '3306')
-DB_NAME = os.getenv('DB_NAME', 'servomonitor')
+# Recuperamos la URL de conexión desde la variable de entorno "DATABASE_URL"
+# Render te proporcionará esta cadena cuando crees tu base de datos PostgreSQL.
+# Ejemplo de cadena: "postgresql://usuario:password@host:puerto/nombre_bd"
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://root:VRVuGa8Acji8WwOKOC98NDEZ1vFfElIA@dpg-cv2dlsd2ng1s738p1ncg-a/servomonitor_xglp")
 
-SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
